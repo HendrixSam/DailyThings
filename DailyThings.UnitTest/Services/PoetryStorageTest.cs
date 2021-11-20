a8ec072fc3cb605972107dbb5c222e0fd14cd2d6
denghui hao <1461246235@qq.com>
2021-11-20 16:57:47 +0800

    添加单元测试

---
+++ DailyThings.UnitTest/Services/PoetryStorageTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyThings.UnitTest.Services {
+    /// <summary>
+    /// 诗词存储测试
+    /// </summary>
+    public class PoetryStorageTest {
+
+    }
+}
