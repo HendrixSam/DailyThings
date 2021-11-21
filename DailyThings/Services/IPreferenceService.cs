a8a67ff48a0ae62dc1d98d7ca8fa7fb76fc2349b
denghui hao <1461246235@qq.com>
2021-11-21 14:35:40 +0800

    新增IPreferenceService偏好存储Service

---
+++ DailyThings/Services/IPreferenceService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyThings.Services {
+    public interface IPreferenceService {
+        /// <param name="key">Preference key.</param>
+        /// <param name="value">Preference value.</param>
+        /// <summary>Sets a value for a given key.</summary>
+        void Set(string key, string value);
+
+        /// <param name="key">Preference key.</param>
+        /// <param name="defaultValue">Default value to return if the key does not exist.</param>
+        /// <summary>Gets the value for a given key, or the default specified if the key does not exist.</summary>
+        void Get(string key, string defaultValue);
+    }
+}
