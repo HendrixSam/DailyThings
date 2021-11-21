6fe11757f9ac38351cb75be3bff65bcc6fac1078
denghui hao <1461246235@qq.com>
2021-11-21 15:01:38 +0800

    更新PoetryStorageTest

--- DailyThings.UnitTest/Services/PoetryStorageTest.cs
+++ DailyThings.UnitTest/Services/PoetryStorageTest.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using DailyThings.Services;
 using DailyThings.Services.Implementations;
+using Moq;
 using NUnit.Framework;
 using Xamarin.Essentials;
 
@@ -11,19 +13,25 @@ namespace DailyThings.UnitTest.Services {
     /// <summary>
     /// 诗词存储测试
     /// </summary>
-    public class PoetryStorageTest : SystemException {
+    public class PoetryStorageTest {
+        [SetUp, TearDown] //SetUp单元测试运行前运行，TearDown单元测试运行后运行
+        public static void RemoveDatabaseFile() {
+            File.Delete(PoetryStorage.PoetryDbPath); //自动删除单元测试的文件
+        }
+
         /// <summary>
         /// 测试初始化诗词存储
         /// </summary>
         [Test]
         public async Task TestInitializeAsync() {
-            //Preferences.Set("key", "value");
-            Assert.IsFalse(File.Exists(PoetryStorage.PoetryDbPath));//没有初始化前PoetryDbPath一定不存在
-            var poetryStorage = new PoetryStorage();
-            await poetryStorage.InitializeAsync();//进行初始化
+            Assert.IsFalse(
+                File.Exists(PoetryStorage
+                    .PoetryDbPath)); //没有初始化前PoetryDbPath一定不存在
+            var preferenceStorageMock = new Mock<IPreferenceService>(); //mock工具
+            var mockPreferenceService = preferenceStorageMock.Object; //得到的mock
+            var poetryStorage = new PoetryStorage(mockPreferenceService);
+            await poetryStorage.InitializeAsync(); //进行初始化
             Assert.IsTrue(File.Exists(PoetryStorage.PoetryDbPath));
-            
         }
     }
-
 }
