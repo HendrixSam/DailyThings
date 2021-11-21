eaa324b7a3b142cf49565baa7044f09019909afe
denghui hao <1461246235@qq.com>
2021-11-21 14:44:06 +0800

    更新IPreferenceService

--- DailyThings/Services/IPreferenceService.cs
+++ DailyThings/Services/IPreferenceService.cs
@@ -10,8 +10,18 @@ namespace DailyThings.Services {
         void Set(string key, string value);
 
         /// <param name="key">Preference key.</param>
+        /// <param name="value">Preference value.</param>
+        /// <summary>Sets a value for a given key.</summary>
+        void Set(string key, int value);
+
+        /// <param name="key">Preference key.</param>
         /// <param name="defaultValue">Default value to return if the key does not exist.</param>
         /// <summary>Gets the value for a given key, or the default specified if the key does not exist.</summary>
-        void Get(string key, string defaultValue);
+        string Get(string key, string defaultValue);
+
+        /// <param name="key">Preference key.</param>
+        /// <param name="defaultValue">Default value to return if the key does not exist.</param>
+        /// <summary>Gets the value for a given key, or the default specified if the key does not exist.</summary>
+        int Get(string key, int defaultValue);
     }
 }
