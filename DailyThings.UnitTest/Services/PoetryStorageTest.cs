e9e43c4cdd00ee1262d7c2669771c7a61e5e535d
denghui hao <1461246235@qq.com>
2021-11-21 15:18:02 +0800

    完成TestIsInitialized以及TestIsNotInitialized

--- DailyThings.UnitTest/Services/PoetryStorageTest.cs
+++ DailyThings.UnitTest/Services/PoetryStorageTest.cs
@@ -32,9 +32,41 @@ namespace DailyThings.UnitTest.Services {
             var poetryStorage = new PoetryStorage(mockPreferenceService);
             await poetryStorage.InitializeAsync(); //进行初始化
             Assert.IsTrue(File.Exists(PoetryStorage.PoetryDbPath));
-            preferenceStorageMock.Verify(p =>
-                p.Set(PoetryStorageConstants.VersionKey,
-                    PoetryStorageConstants.Version), Times.Once);// 测试版本号的set是否调用过一次
+            preferenceStorageMock.Verify(
+                p => p.Set(PoetryStorageConstants.VersionKey,
+                    PoetryStorageConstants.Version),
+                Times.Once); // 测试版本号的set是否调用过一次
+        }
+
+        /// <summary>
+        /// 测试诗词存储是否已经初始化
+        /// </summary>
+        [Test]
+        public void TestIsInitialized() {
+            var preferenceStorageMock = new Mock<IPreferenceStorage>();
+            preferenceStorageMock
+                .Setup(p => p.Get(PoetryStorageConstants.VersionKey,
+                    PoetryStorageConstants.DefaultVersion))
+                .Returns(PoetryStorageConstants
+                    .Version); // 如果调用了Get方法就返回PoetryStorageConstants.Version
+            var mockPreferenceService = preferenceStorageMock.Object;
+            var poetryStorage = new PoetryStorage(mockPreferenceService);
+            Assert.IsTrue(poetryStorage.Initialized());
+        }
+
+        /// <summary>
+        /// 测试诗词存储没有被初始化
+        /// </summary>
+        [Test]
+        public void TestIsNotInitialized() {
+            var preferenceStorageMock = new Mock<IPreferenceStorage>();
+            preferenceStorageMock
+                .Setup(p => p.Get(PoetryStorageConstants.VersionKey,
+                    PoetryStorageConstants.DefaultVersion))
+                .Returns(PoetryStorageConstants.Version - 1);
+            var mockPreferenceService = preferenceStorageMock.Object;
+            var poetryStorage = new PoetryStorage(mockPreferenceService);
+            Assert.IsFalse(poetryStorage.Initialized());
         }
     }
 }
