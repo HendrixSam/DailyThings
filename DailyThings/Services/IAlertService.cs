6c8777da19e6efa9a8eb1238fc18c61a7b9f5821
denghui hao <1461246235@qq.com>
2021-11-22 11:50:05 +0800

    添加警告服务的接口与实现

---
+++ DailyThings/Services/IAlertService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyThings.Services {
+    /// <summary>
+    /// 警告服务
+    /// </summary>
+    public interface IAlertService {
+        /// <summary>
+        /// 显示警告
+        /// </summary>
+        /// <param name="title">标题</param>
+        /// <param name="content">内容</param>
+        /// <param name="button">按钮文字</param>
+        void DisPlayAlert(string title, string content, string button);
+    }
+}
---
+++ DailyThings/Services/Implementations/AlertService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DailyThings.Services.Implementations {
+    public class AlertService : IAlertService {
+        /******** 私有变量 ********/
+
+        /// <summary>
+        /// 用于显示警告的MainPage。
+        /// </summary>
+        private MainPage MainPage =>
+            _mainPage ?? (_mainPage = Application.Current.MainPage as MainPage);
+
+        /// <summary>
+        /// 用于显示警告的MainPage。
+        /// </summary>
+        private MainPage _mainPage;
+
+        /******** 继承方法 ********/
+
+        /// <summary>
+        /// 显示警告
+        /// </summary>
+        /// <param name="title">标题</param>
+        /// <param name="content">内容</param>
+        /// <param name="button">按钮文字</param>
+        public void DisPlayAlert(string title, string content, string button) {
+            MainPage.DisplayAlert(title, content, button);
+        }
+    }
+}
