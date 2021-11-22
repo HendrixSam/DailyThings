f12da56355b7467fed1d28ccfe1a5041653b50c6
denghui hao <1461246235@qq.com>
2021-11-22 13:34:51 +0800

    完成PoetryService的运行测试

--- DailyThings/Services/Implementations/PreferenceStorage.cs
+++ DailyThings/Services/Implementations/PreferenceStorage.cs
@@ -27,7 +27,7 @@ namespace DailyThings.Services.Implementations {
 
         /// <param name="key">Preference key.</param>
         /// <param name="defaultValue">Default value to return if the key does not exist.</param>
-        /// <summary>Gets the value for a given key, or the default spewcified if the key does not exist.</summary>
+        /// <summary>Gets the value for a given key, or the default specified if the key does not exist.</summary>
         public int Get(string key, int defaultValue) =>
             Preferences.Get(key, defaultValue);
     }
--- DailyThings/Views/MainPage.xaml.cs
+++ DailyThings/Views/MainPage.xaml.cs
@@ -1,9 +1,23 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Runtime.InteropServices;
+using DailyThings.Services;
+using DailyThings.Services.Implementations;
+using GalaSoft.MvvmLight.Ioc;
+using Xamarin.Forms;
 
 namespace DailyThings {
     public partial class MainPage : ContentPage {
         public MainPage() {
             InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e) {
+            PoetryService poetryService = new PoetryService(
+                SimpleIoc.Default.GetInstance<IPreferenceStorage>(),
+                SimpleIoc.Default.GetInstance<IAlertService>(),
+                SimpleIoc.Default.GetInstance<IPoetryStorage>());
+            var poetry = await poetryService.GetPoetryAsync();
+            Result.Text = poetry.Name;
         }
     }
 }
