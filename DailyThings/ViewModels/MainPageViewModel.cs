dfecfa1c65f894602855ac6d59f932a47bad5152
denghui hao <1461246235@qq.com>
2021-11-22 09:31:48 +0800

    新建MainPageViewModel并进行注册

---
+++ DailyThings/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyThings.ViewModels {
+    /// <summary>
+    /// 主页面
+    /// </summary>
+    public class MainPageViewModel {
+    }
+}
--- DailyThings/ViewModels/ViewModelLocator.cs
+++ DailyThings/ViewModels/ViewModelLocator.cs
@@ -1,4 +1,24 @@
-﻿namespace DailyThings.ViewModels {
+﻿using DailyThings.Services;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace DailyThings.ViewModels {
+    /// <summary>
+    /// ViewModel Locator
+    /// </summary>
     public class ViewModelLocator {
+        /// <summary>
+        /// 主页
+        /// </summary>
+        public MainPageViewModel MainPageViewModel =>
+            SimpleIoc.Default.GetInstance<MainPageViewModel>();
+
+        /// <summary>
+        /// ViewModel Locator
+        /// </summary>
+        public ViewModelLocator() {
+            //注册
+            SimpleIoc.Default.Register<IPoetryStorage, IPoetryStorage>();
+            SimpleIoc.Default.Register<MainPageViewModel>();
+        }
     }
 }
