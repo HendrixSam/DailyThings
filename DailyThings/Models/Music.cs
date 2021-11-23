d9aa44a528fe5556f4221e3cfba1e63147f39e74
denghui hao <1461246235@qq.com>
2021-11-23 11:59:56 +0800

    将数据库更新为DailyThings.db

--- DailyThings/Models/Music.cs
+++ DailyThings/Models/Music.cs
@@ -6,25 +6,37 @@ namespace DailyThings.Models {
     /// <summary>
     /// 音乐类
     /// </summary>
+    [SQLite.Table("Music")]
     public class Music {
         /// <summary>
         /// 音乐Id
         /// </summary>
-        public int SongId { get; set; }
+        [SQLite.PrimaryKey]
+        [SQLite.Column("id")]
+        public int Id { get; set; }
 
         /// <summary>
         /// 音乐名称
         /// </summary>
-        public string SongName { get; set; }
+        [SQLite.Column("name")]
+        public string Name { get; set; }
 
         /// <summary>
         /// 艺术家
         /// </summary>
+        [SQLite.Column("artist")]
         public string Artist { get; set; }
+
+        /// <summary>
+        /// 标签
+        /// </summary>
+        [SQLite.Column("match_tags")]
+        public string MatchTags { get; set; }
 
         /// <summary>
         /// Url
         /// </summary>
+        [SQLite.Ignore]
         public string Url { get; set; }
     }
 }
