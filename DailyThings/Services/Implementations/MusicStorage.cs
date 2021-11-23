b2f053ef154d7c6d729586cb81b4d592914c9d46
denghui hao <1461246235@qq.com>
2021-11-23 19:11:59 +0800

    完成MusicStorage

--- DailyThings/Services/Implementations/MusicStorage.cs
+++ DailyThings/Services/Implementations/MusicStorage.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using DailyThings.Models;
 using DailyThings.Services.Implementations;
 
-namespace DailyThings.Services {
+namespace DailyThings.Services.Implementations {
     public class MusicStorage : DataBaseService{
+        /******** 公有方法 ********/
+
+        /// <summary>
+        /// 获取一首音乐
+        /// </summary>
+        /// <param name="matchTags">音乐标签</param>
+        public async Task<IList<Music>> GetMusicAsync(string matchTags) =>
+            await Connection.Table<Music>()
+                .Where(m => m.MatchTags == matchTags).ToListAsync();
+
+
         /// <summary>
         /// 构造方法
         /// </summary>
