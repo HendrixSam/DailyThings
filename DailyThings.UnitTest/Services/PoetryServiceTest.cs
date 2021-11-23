1e04401093504dd995b40794d2f09c04fda18cbf
denghui hao <1461246235@qq.com>
2021-11-23 08:47:19 +0800

    添加PoetryServiceTest单元测试

---
+++ DailyThings.UnitTest/Services/PoetryServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyThings.UnitTest.Services {
+    /// <summary>
+    /// 诗词服务测试
+    /// </summary>
+    public class PoetryServiceTest {
+    }
+}
